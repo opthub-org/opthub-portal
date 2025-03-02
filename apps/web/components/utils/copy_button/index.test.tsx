3aa05e702966015d15629ea92f21c5736c78beed
三嶋 隆史 <ryuji.mishima.com@gmail.com>
2025-03-03 01:57:45 +0900

    chore: 不要なコンポーネントを削除

--- apps/web/components/utils/copy_button/index.test.tsx
+++
@@ -1,5 +0,0 @@
-import { componentDomSnapshotTest } from '@/test/component'
-import { CopyButton } from '.'
-import { Cases } from './cases'
-
-componentDomSnapshotTest('CopyButton', CopyButton, Cases)
